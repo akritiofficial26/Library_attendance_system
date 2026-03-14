2a22a5a80b3660b8a6883af1163e091b1d5f6bcf
aniketofficial540 <160538419+aniketofficial540@users.noreply.github.com>
2026-03-14 16:34:06 +0530

    fixed the prisma generate issue

---
+++ backend/prisma.config.mjs
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+import { defineConfig, env } from 'prisma/config';
+
+dotenv.config();
+
+export default defineConfig({
+  schema: './prisma/schema.prisma',
+  datasource: {
+    url: env('DATABASE_URL'),
+    directUrl: env('DIRECT_URL'),
+  },
+});
